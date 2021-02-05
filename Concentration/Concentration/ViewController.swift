85d289e5775ba1ae06143589708e20db12fc7e79
Viktor Kulia <vitya.kulya@gmail.com>
2021-02-05 15:18:03 +0200

    third commit
    I have been implemented some new buttons and created a flip count

--- Concentration/Concentration/ViewController.swift
+++ Concentration/Concentration/ViewController.swift
@@ -8,20 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var flipCount = 0
+    { didSet {
+        flipCountLabel.text = "Flips: \(flipCount)"
+    } }
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    var emojiChoices = ["🐶", "🐱", "🐶", "🐱"]
+    @IBOutlet var cardButtons: [UIButton]!
+    
+//    @IBAction func newGameButton(_ sender: UIButton) {
+//        let flipCount = 0
+//        flipCountLabel.text = "Flips: \(flipCount)"
+//    }
+//    I will add this buttom soon
+    
     @IBAction func touchCard(_ sender: UIButton) {
-        flipCard(withEmoji: "🐶", on: sender)
+        flipCount += 1
+        if let cardNumber = cardButtons.firstIndex(of: sender){
+        flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        } else {
+            print ("choosen card was not in cardButtons")
+        }
     }
     
-    @IBAction func touchSecondCard(_ sender: UIButton) {
-        flipCard(withEmoji: "🐱", on: sender)
-    }
     func flipCard(withEmoji emoji: String, on button: UIButton){
-        print("flipCard (withEmoji \("emoji"))")
-        
+
         if button.currentTitle == emoji {
             button.setTitle("", for: UIControl.State.normal)
-            button.backgroundColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.7959558368, blue: 0, alpha: 1)
         } else {
             button.setTitle(emoji, for: UIControl.State.normal)
             button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
