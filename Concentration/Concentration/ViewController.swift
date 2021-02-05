2cb69463cf09765aa5f2325ae026d26f5134b987
Viktor Kulia <vitya.kulya@gmail.com>
2021-02-05 11:16:03 +0200

    second commit
    added 2 buttons

--- Concentration/Concentration/ViewController.swift
+++ Concentration/Concentration/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCard(withEmoji: "🐶", on: sender)
     }
-
+    
+    @IBAction func touchSecondCard(_ sender: UIButton) {
+        flipCard(withEmoji: "🐱", on: sender)
+    }
+    func flipCard(withEmoji emoji: String, on button: UIButton){
+        print("flipCard (withEmoji \("emoji"))")
+        
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
 
 }
 
